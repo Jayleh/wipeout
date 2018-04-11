26364d3e70a9a085fd42f78248b4804791dffeab
Jayleh <jaylehyun@gmail.com>
2018-04-11 10:03:12 -0700

    added stations and tobs routes to flask api

--- app.py
+++ app.py
@@ -32,11 +32,11 @@ def home():
     """
     return (
         f"Available Routes:<br/><br/>"
-        f"/api/v1.0/precipitation — List of precipitation data<br/>"
+        f"/api/v1.0/precipitation — List of precipitation (prcp) data for 2017<br/>"
         f"/api/v1.0/stations — List of stations<br/>"
-        f"/api/v1.0/tobs — List of temperature observations (tobs)<br/>"
+        f"/api/v1.0/tobs — List of temperature observations (tobs) for 2017<br/>"
         f"/api/v1.0/start-date/end-date"
-        f" — list of the minimum temperature, the average temperature, and the max temperature "
+        f" — List of the minimum, average, and max temperatures "
         "for a given start or start-end range<br/>"
     )
 
@@ -50,7 +50,7 @@ def precipitation():
     # Convert list of tuples into normal list
     # prcp_list = list(np.ravel(results))
 
-    # Create a list of dicts with `date` and `prcp` as the keys and
+    # Create a list of dicts with `date` and `prcp` as the keys
     prcp_data = []
     for result in results:
         row = {}
@@ -58,19 +58,50 @@ def precipitation():
         row["prcp"] = float(result[1])
         prcp_data.append(row)
 
+    print("Server received request for 'About' page...")
     return jsonify(prcp_data)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
-    # jsonify(dictionary)
-    pass
+    # Query stations data
+    results = session.query(Station.station, Station.name, Station.latitude,
+                            Station.longitude, Station.elevation).all()
+
+    # Create lists of dicts
+    station_data = []
+    for result in results:
+        row = {}
+        row["station"] = result[0]
+        row["name"] = result[1]
+        row["latitude"] = result[2]
+        row["longitude"] = result[3]
+        row["elevation"] = result[4]
+        station_data.append(row)
+
+    print("Server received request for 'About' page...")
+    return jsonify(station_data)
 
 
 @app.route("/api/v1.0/tobs")
 def tobs():
-    # jsonify(dictionary)
-    pass
+    # Query precipitation data from last year
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= "2017-01-01").all()
+
+    # Convert list of tuples into normal list
+    # prcp_list = list(np.ravel(results))
+
+    # Create a list of dicts with `date` and `tobs` as the keys
+    tobs_data = []
+    for result in results:
+        row = {}
+        row["date"] = result[0]
+        row["tobs"] = float(result[1])
+        tobs_data.append(row)
+
+    print("Server received request for 'About' page...")
+    return jsonify(tobs_data)
 
 
 @app.route("/api/v1.0/<start>")
